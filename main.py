7f9f479565ec1587859c7d24e6c897584fad0112
dakotachasesmith <dakotachasesmith@gmail.com>
2020-05-23 15:42:31 -1000

    Add lighting to scene

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from direct.actor.Actor import Actor
 from direct.showbase.ShowBase import ShowBase
+from panda3d.core import AmbientLight
+from panda3d.core import DirectionalLight
 from panda3d.core import WindowProperties
+from panda3d.core import Vec4
 
 class Game(ShowBase):
 	def __init__(self):
@@ -18,15 +21,24 @@ class Game(ShowBase):
 		self.environment = loader.loadModel("Models/Misc/environment")
 		self.environment.reparentTo(render)
 
+		# Load in some lights to spice things up
+		# ambientLight = AmbientLight("ambient light")
+		# ambientLight.setColor(Vec4(0.2, 0.2, 0.2, 1))
+		# self.ambientLightNodePath = render.attachNewNode(ambientLight)
+		# render.setLight(self.ambientLightNodePath)
+
+		# Kind of dark with just an ambient light, maybe a directional light eh
+		mainLight = DirectionalLight("main light")
+		self.mainLightNodePath = render.attachNewNode(mainLight)
+		self.mainLightNodePath.setHpr(45, -45, 0)
+		render.setLight(self.mainLightNodePath)
+
+		# hook us up some automatic perpixel lighting
+		render.setShaderAuto()
+
 		# Load an animated model
 		self.tempActor = Actor("Models/PandaChan/act_p3d_chan", {"walk" : "Models/PandaChan/a_p3d_chan_run"})
 		self.tempActor.reparentTo(render)
-
-		# Move the actor to a position where it is visible by the default camera
-		self.tempActor.setPos(0, 7, 0)
-
-		# Rotate the actor's orientation (heading) from 0 to 180 (now facing away from camera)
-		self.tempActor.getChild(0).setH(180)
 
 		# Set an animation for the actor
 		self.tempActor.loop("walk")
